ead688a681c2965e590b43e09c957d21e4ecad8d
florianthiery <web@florian-thiery.de>
2019-10-07 11:59:55 +0200

    Update ogham.py

--- ogham.py
+++ ogham.py
@@ -1,8 +1,12 @@
 # python [path]get-pip.py (https://www.liquidweb.com/kb/install-pip-windows/)
 # pip install sparqlwrapper
 # https://rdflib.github.io/sparqlwrapper/
+# pip install geojson
+
 
 from SPARQLWrapper import SPARQLWrapper, JSON
+import geojson
+import tempfile
 
 endpoint_url = "https://query.wikidata.org/sparql"
 
@@ -34,3 +38,11 @@ results = get_results(endpoint_url, query)
 
 for result in results["results"]["bindings"]:
     print(result)
+
+def write_json(self, features):
+   # feature is a shapely geometry type
+   geom_in_geojson = geojson.Feature(geometry=features, properties={})
+   tmp_file = tempfile.mkstemp(suffix='.geojson')
+   with open(tmp_file[1], 'w') as outfile:
+      geojson.dump(geom_in_geojson, outfile)
+   return tmp_file[1]
