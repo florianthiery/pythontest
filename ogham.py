64967f1afb1c6f0d5d08550aab676f1137a7099c
florianthiery <web@florian-thiery.de>
2019-10-07 13:32:59 +0200

    Update ogham.py

--- ogham.py
+++ ogham.py
@@ -1,16 +1,21 @@
 # python [path]get-pip.py (https://www.liquidweb.com/kb/install-pip-windows/)
+# pip install pandas
+# conda install geopandas with Anaconda https://geopandas.readthedocs.io/en/latest/install.html#installing-with-anaconda-conda
 # pip install sparqlwrapper
 # https://rdflib.github.io/sparqlwrapper/
 # pip install geojson
+# pip install shapely
 
-
+import pandas as pd
+#import geopandas
 from SPARQLWrapper import SPARQLWrapper, JSON
 import geojson
 import tempfile
+#from shapely import wkt
 
 endpoint_url = "https://query.wikidata.org/sparql"
 
-query = """SELECT * WHERE {
+query = """SELECT ?label ?geo ?item WHERE {
   ?item wdt:P31 wd:Q2016147;
     wdt:P361 wd:Q67978809;
     wdt:P195 ?collection.
@@ -36,13 +41,35 @@ def get_results(endpoint_url, query):
 
 results = get_results(endpoint_url, query)
 
-for result in results["results"]["bindings"]:
-    print(result)
+labels = []
+geoms = []
+items = []
 
-def write_json(self, features):
-   # feature is a shapely geometry type
-   geom_in_geojson = geojson.Feature(geometry=features, properties={})
-   tmp_file = tempfile.mkstemp(suffix='.geojson')
-   with open(tmp_file[1], 'w') as outfile:
-      geojson.dump(geom_in_geojson, outfile)
-   return tmp_file[1]
+for result in results["results"]["bindings"]:
+    #print(result)
+    #print(result["label"]["value"])
+    #print(result["geo"]["value"])
+    #print(result["item"]["value"])
+    labels.append(result["label"]["value"])
+    geoms.append(result["geo"]["value"])
+    items.append(result["item"]["value"])
+
+#print(labels)
+
+# dataframe and pandas
+# https://www.geeksforgeeks.org/python-pandas-dataframe/
+
+# Define a dictionary containing employee data
+data = {'labels':labels,
+        'geoms':geoms,
+        'items':items}
+
+# Convert the dictionary into DataFrame
+df = pd.DataFrame(data)
+
+# select two columns
+print(df[['labels', 'items', 'geoms']])
+
+# geojson stuff
+
+# https://geopandas.readthedocs.io/en/latest/gallery/create_geopandas_from_pandas.html
